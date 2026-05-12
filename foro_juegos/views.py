fefc28afe0407bb252b2d371618ee9400f0c0d27
Franco <f.gentile@alumno.um.edu.ar>
2026-05-12 19:23:41 -0300

    Agregar paginacion a todos los endpoints API

--- foro_juegos/views.py
+++ foro_juegos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from rest_framework.pagination import PageNumberPagination
 from .models import User, Category, Post, Comment, Image, Report
 from .serializers import (
     UserSerializer, UserCreateSerializer,
@@ -10,9 +11,16 @@ from .serializers import (
 )
 
 
+class StandardPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardPagination
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -23,11 +31,13 @@ class UserViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = StandardPagination
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardPagination
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -39,13 +49,16 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardPagination
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    pagination_class = StandardPagination
 
 
 class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    pagination_class = StandardPagination
